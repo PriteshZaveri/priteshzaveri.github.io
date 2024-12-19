e595025d52a0f675fcdac39cd6874d76c958ee62
Pritesh Zaveri <priteshjhaveri27@gmail.com>
2024-12-19 16:48:24 -0500

    Updated all dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-require("dotenv").config();
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// This is the ID of the div in your index.html file
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+    <App />
+);
